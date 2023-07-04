f8f3478d56e87fb5f94109a0d2a071ae6b3bfb9e
BuildTools <nickrmanha@gmail.com>
2023-07-04 10:00:22 -0700

    utd

--- src/main.jsx
+++ src/main.jsx
@@ -1,39 +1,54 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  Route,
+  createBrowserRouter,
+  RouterProvider,
+  createRoutesFromElements,
+} from "react-router-dom";
 import "./index.css";
 import Root from "./pages/root";
 import Home from "./pages/Home";
 import About from "./pages/About";
-import Contact from "./pages/Contact";
+// import Contact from "./pages/Contact";
 import Experience from "./pages/Experience.jsx";
+import ScrollToTop from "./ScrollToTop";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
+const router = createBrowserRouter(
+  //   [
+  //   {
+  //     path: "/",
+  //     element: <Root />,
 
-    children: [
-      {
-        path: "/",
-        element: <Home />,
-      },
+  //     children: [
+  //       {
+  //         path: "/",
+  //         element: <Home />,
+  //       },
 
-      {
-        path: "/about",
-        element: <About />,
-      },
-      {
-        path: "/contact",
-        element: <Contact />,
-      },
-      {
-        path: "/experience",
-        element: <Experience />,
-      },
-    ],
-  },
-]);
+  //       {
+  //         path: "/about",
+  //         element: <About />,
+  //       },
+  //       {
+  //         path: "/contact",
+  //         element: <Contact />,
+  //       },
+  //       {
+  //         path: "/experience",
+  //         element: <Experience />,
+  //       },
+  //     ],
+  //   },
+  // ]
+  createRoutesFromElements(
+    <Route path="/" element={<Root />}>
+      <Route index element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="experience" element={<Experience />} />
+    </Route>
+  )
+);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
