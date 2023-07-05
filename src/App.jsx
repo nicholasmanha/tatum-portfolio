de49972d95d35f8d81bf2946298c922a36f80912
Nicholas Manha <nickrmanha@gmail.com>
2023-07-05 11:16:54 -0700

    scrolltotop works

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Root from "./pages/root"
+import Contact from "./pages/Contact"
+import Experience from "./pages/Experience"
+
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Root />}>
+        <Route index element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/experience" element={<Experience />} />
+
+      </Route>
+
+      
+      
+    </Routes>
+  )
+}
+
+export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,57 +1,59 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import {
-  Route,
-  createBrowserRouter,
-  RouterProvider,
-  createRoutesFromElements,
+  // Route,
+  // createBrowserRouter,
+  // RouterProvider,
+  // createRoutesFromElements,
+  BrowserRouter,
 } from "react-router-dom";
-import "./index.css";
-import Root from "./pages/root";
-import Home from "./pages/Home";
-import About from "./pages/About";
-// import Contact from "./pages/Contact";
-import Experience from "./pages/Experience.jsx";
+// import "./index.css";
+import App from "./App"
 import ScrollToTop from "./ScrollToTop";
+// import Root from "./pages/root";
+// import Home from "./pages/Home";
+// import About from "./pages/About";
+// // import Contact from "./pages/Contact";
+// import Experience from "./pages/Experience.jsx";
+// import ScrollToTop from "./ScrollToTop";
 
-const router = createBrowserRouter(
-  //   [
-  //   {
-  //     path: "/",
-  //     element: <Root />,
+// const router = createBrowserRouter(
+//   //   [
+//   //   {
+//   //     path: "/",
+//   //     element: <Root />,
 
-  //     children: [
-  //       {
-  //         path: "/",
-  //         element: <Home />,
-  //       },
+//   //     children: [
+//   //       {
+//   //         path: "/",
+//   //         element: <Home />,
+//   //       },
 
-  //       {
-  //         path: "/about",
-  //         element: <About />,
-  //       },
-  //       {
-  //         path: "/contact",
-  //         element: <Contact />,
-  //       },
-  //       {
-  //         path: "/experience",
-  //         element: <Experience />,
-  //       },
-  //     ],
-  //   },
-  // ]
-  createRoutesFromElements(
-    <Route path="/" element={<Root />}>
-      <Route index element={<Home />} />
-      <Route path="about" element={<About />} />
-      <Route path="experience" element={<Experience />} />
-    </Route>
-  )
-);
+//   //       {
+//   //         path: "/about",
+//   //         element: <About />,
+//   //       },
+//   //       {
+//   //         path: "/contact",
+//   //         element: <Contact />,
+//   //       },
+//   //       {
+//   //         path: "/experience",
+//   //         element: <Experience />,
+//   //       },
+//   //     ],
+//   //   },
+//   // ]
+  
+// );
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <BrowserRouter>
+      <ScrollToTop />
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
