b06e98f79ab4ebbc019643bd04e1574b3ea0ad2b
Nicholas Manha <nickrmanha@gmail.com>
2024-02-07 20:49:50 -0800

    removed lorem ipsum

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -132,30 +132,7 @@ function Experience() {
             {/*  */}
           </div>
           <p className="mb-5">
-            Lorem ipsum dolor amet consectetur adipisicing elit. Modi enim
-            minima nobis officia eum placeat rerum repellendus quasi dolorem,
-            rem magnam, aliquid, sunt deleniti. Corporis quisquam quasi nulla
-            voluptatum, assumenda fugiat dolor facere dignissimos! Nostrum sint
-            deleniti consectetur culpa tempora at, voluptates rem doloribus. Non
-            placeat vero amet quod. Non, est quasi nobis dignissimos repellat
-            tenetur, voluptas, nesciunt natus laborum quibusdam aliquam! Amet
-            voluptatum, voluptatibus eveniet inventore, accusamus quod omnis non
-            fugiat reiciendis necessitatibus aut iusto facere vel quasi dolore!
-            Saepe necessitatibus in voluptas quae! Voluptatibus beatae nulla
-            ducimus laboriosam, sapiente impedit quibusdam assumenda at tempora
-            reprehenderit ex consectetur ipsum, aperiam recusandae optio
-            officiis, ab nihil non adipisci ullam repudiandae ad. Quae,
-            provident ratione? Beatae quam minima unde voluptate molestiae,
-            voluptas pariatur illum nisi modi aliquam culpa architecto vero
-            magni dolores, iste laboriosam accusantium omnis praesentium.
-            Deleniti, sint! Facilis nisi esse placeat libero natus recusandae.
-            Nobis officiis cum voluptas tenetur vero error, in accusantium totam
-            ut nostrum cumque nam sequi ratione adipisci, necessitatibus ex
-            aliquam mollitia maxime iure deserunt velit maiores aspernatur
-            deleniti. Eligendi, ut at! Aliquid labore fuga eaque, quam excepturi
-            accusantium dignissimos voluptatum cupiditate, architecto facere
-            autem blanditiis quidem quas repudiandae. Vitae sunt, asperiores
-            assumenda eaque illum nemo.
+            
           </p>
         </div>
         <h1 className="mb-5 text-uppercase font-weight-bold">FSC</h1>
